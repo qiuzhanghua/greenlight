8d492ac53ccae40e19e92011ceb309f10df072b7
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-22 17:41:33 +0800

    sort asc/desc

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,31 @@
 package data
 
-import "greenlight.taiji.dev/internal/validator"
+import (
+	"greenlight.taiji.dev/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
