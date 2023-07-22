a6be7dc1250d3ccd864411e016b8f7c5a0feec9f
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-22 20:29:18 +0800

    panic recovery

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
@@ -16,5 +16,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
-	return router
+	return app.recoverPanic(router)
 }
