76058f9c2dfdb8f506ff25f3bf4d1b8bb908b2a0
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-22 20:25:16 +0800

    json log

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"greenlight.taiji.dev/internal/data"
-	"log"
+	"greenlight.taiji.dev/internal/jsonlog"
 	"net/http"
 	"os"
 	"time"
@@ -28,7 +28,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -43,14 +43,14 @@ func main() {
 
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err.Error(), nil)
 	}
 	defer db.Close()
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -65,9 +65,10 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server",
+		map[string]string{"env": cfg.env, "addr": srv.Addr})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err.Error(), nil)
 
 }
 
