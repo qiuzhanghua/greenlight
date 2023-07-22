31e9539d810607258f0db0f2a1751b983045ac9a
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-22 17:35:23 +0800

    full text search

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -134,9 +134,9 @@ func (m MovieModel) Delete(id int64) error {
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	query := `
-		SELECT id, created_at, title, year, runtime, genres, version FROM movies
-		WHERE (LOWER(title) = LOWER($1) OR $1 = '')
-		AND (genres @> $2 OR $2 = '{}')
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
